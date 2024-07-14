8aec2615c4460ba52d356fd3aa282bf0d6744656
Ashhhleyyy <ash@ashhhleyyy.dev>
2024-07-15 00:27:47 +0100

    chore: add nix flake

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem(system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+        inherit (pkgs) lib;
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            (python312.withPackages(ps: with ps; [
+              mkdocs
+              mkdocs-material
+            ]))
+          ];
+        };
+      }
+    );
+}
+
